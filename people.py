6c6877f0a7d159120a61b3ab7189808178246fef
Nadine Siccion <nv.siccion@gmail.com>
2024-02-12 21:53:59 +0800

    Make read_all and read_one compatible with db

--- people.py
+++ people.py
@@ -1,32 +1,13 @@
 # people.py
 
-from datetime import datetime
 from flask import abort, make_response
 
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
-
-PEOPLE = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    }
-}
+from config import db
+from models import Person, people_schema, person_schema
 
 def read_all():
-    return list(PEOPLE.values())
+    people = Person.query.all()
+    return people_schema.dump(people)
 
 def create(person):
     lname = person.get("lname")
@@ -46,8 +27,9 @@ def create(person):
         )
 
 def read_one(lname):
-    if lname in PEOPLE:
-        return PEOPLE[lname]
+    person = Person.query.filter(Person.lname == lname).one_or_none()
+    if person is not None:
+        return person_schema.dump(person)
     else:
         abort(
             404, f"Person with last name {lname} not found"
